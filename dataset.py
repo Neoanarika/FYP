3564efbdc7df34fc73a977503192055f5d722063
Ang Ming Liang <angmingliang4017ic@gmail.com>
2022-09-07 00:17:17 +0800

    Udpated dataset and added gitignore

--- dataset.py
+++ dataset.py
@@ -3,9 +3,9 @@ import jax.numpy as jnp
 from sklearn.datasets import make_circles
 from sklearn.model_selection import train_test_split
 
-def make_linearly_seperable_dataset():
-    X, y = make_circles(n_samples=1_000, factor=0.3, noise=0.05, random_state=0)
-    X_1, X_2, y_1, y_2 = train_test_split(X, y, test_size=0.5, random_state=42)
+def make_linearly_seperable_dataset(n_samples, task_2_fraction=0.5, noise_level = 0.05, seed=0):
+    X, y = make_circles(n_samples=n_samples, factor=0.3, noise=noise_level, random_state=seed)
+    X_1, X_2, y_1, y_2 = train_test_split(X, y, test_size=task_2_fraction, random_state=seed)
 
     subspace1 = np.logical_and(np.logical_and(X_1[:, 0] < 0, X_1[:, 1] < 0.8), X_1[:, 1]> 0.3)
     subspace2 = np.logical_and(np.logical_and(X_2[:, 0] > 0, X_2[:, 1] < 1), X_2[:, 1]> 0.8)
